3857cd762e5a503e28e9b74cc71e28d45305e469
Alice Ng <zcahkwn@ucl.ac.uk>
2025-02-18 09:20:32 +0000

    heatmap plot

--- scripts/plot_occ_collusion_2.py
+++ scripts/plot_occ_collusion_2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import comb, sqrt
-from mpl_toolkits.mplot3d import Axes3D  
+# from mpl_toolkits.mplot3d import Axes3D  
 from occenv.constants import FIGURE_DIR 
 from scripts.collusion_prob import collusion_2
 
@@ -21,24 +21,44 @@ def create_3d_plot(x, y, z, xlabel, ylabel, zlabel, title, filename):
     plt.savefig(FIGURE_DIR / filename)
     plt.close(fig)
 
+def creat_heatmap(x, y, z, xlabel, ylabel, title, filename):
+    fig, ax = plt.subplots(figsize=(8, 6))
+    c = ax.pcolormesh(x, y, z, cmap='viridis')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    fig.colorbar(c, ax=ax)
+    plt.savefig(FIGURE_DIR / filename)
+    plt.close(fig)
+
+
 def plot_collude_2():
     N = 10
     x, y = create_meshgrid()
     z = np.vectorize(lambda n1, n2: collusion_2(N, [n1, n2]))(x, y)
-    create_3d_plot(
+    # create_3d_plot(
+    #     x, y, z,
+    #     '$n_1$', '$n_2$', 'Probability of Collusion',
+    #     'Probability of Collusion (m=2) with $N=10$', "collude_2.pdf"
+    # )
+    creat_heatmap(
         x, y, z,
-        '$n_1$', '$n_2$', 'Probability of Collusion',
-        'Probability of Collusion (m=2) with $N=10$', "collude_2.pdf"
+        '$n_1$', '$n_2$',
+        'Heatmap of Probability of Collusion (m=2) with $N=10$', "collude_2.pdf"
     )
 
 def plot_occ_2():
     N = 10
     x, y = create_meshgrid()
     z = np.sqrt(x * y) / N
-    create_3d_plot(
+    # create_3d_plot(
+    #     x, y, z,
+    #     '$n_1$', '$n_2$', r'OCC when m=2',
+    #     r'3D Surface Plot of OCC when m=2', "occ_2.pdf"
+    # )
+    creat_heatmap(
         x, y, z,
-        '$n_1$', '$n_2$', r'OCC when m=2',
-        r'3D Surface Plot of OCC when m=2', "occ_2.pdf"
+        '$n_1$', '$n_2$', r'OCC when m=2', "occ_2.pdf"
     )
 
 if __name__ == "__main__":
