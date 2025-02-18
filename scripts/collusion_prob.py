ad96976a72683e91ac962fd6d6c6fb5fe22b3e3e
Alice Ng <zcahkwn@ucl.ac.uk>
2025-02-18 00:47:52 +0000

    add plot for OCC and collusion when m=2

--- scripts/collusion_prob.py
+++ scripts/collusion_prob.py
@@ -1,8 +1,14 @@
-from math import comb
+"""
+The non-elegant way to calculate the collusion probability for m shards
+"""
 
+from math import comb
+import numpy as np
 
 def collusion_2(N,shard_sizes: list[int]):
     n1, n2 = shard_sizes
+    if n1 + n2 < N:
+        return np.nan
     return comb(n1,n1+n2-N)/comb(N,n2)
 
 def collusion_3(N:int,shard_sizes: list[int]):
@@ -11,7 +17,6 @@ def collusion_3(N:int,shard_sizes: list[int]):
     for i in range(max(0,N-n1-n3),min(N-n1+1,n2+1)):
         sum_ += comb(N-n1,i)*comb(n1,n2-i)*comb(n1+i,n3+n1+i-N)/(comb(N,n2)*comb(N,n3))
     return sum_
-
 # print("for m=3, N=10, [4,3,3], using method 1: ",collusion_3(10,[4,3,3]))
 
 def collusion_3_method_2(N:int,n1,n2,n3):
@@ -19,8 +24,17 @@ def collusion_3_method_2(N:int,n1,n2,n3):
     for i in range(max(N-n1,n3,n2),min(n2+n3,N)+1):
         sum_ += comb(n1,n1+i-N)*comb(i,n2)*comb(n2,n2+n3-i)/(comb(N,n2)*comb(N,n3))
     return sum_
-
 # print("using method 2: ",collusion_3_method_2(10,2,6,3))
+
+def collusion_3_method_3(N:int,shard_sizes: list[int]):
+    sum_=0
+    n1,n2,n3 = shard_sizes
+    def f2(k,n1,n2):
+        return comb(n1,n1+n2-k)/comb(k,n2)
+    for i in range(max(N-n3,n1,n2),min(n1+n2,N)+1):
+        sum_ += comb(n3,n3+i-N)*comb(i,n2)*comb(i,n1)*f2(i,n1,n2)/(comb(N,n2)*comb(N,n1))
+    return sum_
+print("using method 3: ",collusion_3_method_3(10,[2,6,5]))
 
 def collusion_4(N:int, shard_sizes: list[int]):
     n1, n2, n3, n4 = shard_sizes
@@ -40,6 +54,20 @@ def collusion_4_method_2(N:int,shard_sizes: list[int]):
     return sum_
 # print("for m=4: ",collusion_4_method_2(10,[7,2,1,1]))
 
+def collusion_4_method_3(N:int,shard_sizes: list[int]):
+    n1, n2, n3, n4 = shard_sizes
+    sum_=0
+    def f2(k,n1,n2):
+        return comb(n1,n1+n2-k)/comb(k,n2)
+    def f3(k,n1,n2,n3):
+        sum_3=0
+        for j in range(max(k-n3,n1,n2),min(n1+n2,k)+1):
+            sum_3 += comb(n3,n3+j-k)*comb(j,n2)*comb(j,n1)*f2(j,n1,n2)/(comb(k,n2)*comb(k,n1))
+        return sum_3
+    for i in range(max(N-n4,n1,n2,n3),min(n1+n2+n3,N)+1):
+        sum_ += comb(i,n3)*comb(n4,n4+i-N)*comb(i,n1)*comb(i,n2)*f3(i,n1,n2,n3)/(comb(N,n1)*comb(N,n2)*comb(N,n3))
+        return sum_
+# print("using method 3: ",collusion_4_method_3(10,[2,6,5,3]))
 
 def collusion_5(N:int, shard_sizes: list[int]):
     n1, n2, n3, n4, n5 = shard_sizes
@@ -49,7 +77,6 @@ def collusion_5(N:int, shard_sizes: list[int]):
             for k in range(max(0,N-n1-n5-i-j),min(N-n1-i-j+1,n4+1)):
                 sum_ += comb(N-n1,i)*comb(n1,n2-i)*comb(N-n1-i,j)*comb(n1+i,n3-j)*comb(N-n1-i-j,k)*comb(n1+i+j,n4-k)*comb(n1+i+j+k,n5+n1+i+j+k-N)/(comb(N,n2)*comb(N,n3)*comb(N,n4)*comb(N,n5))
     return sum_
-
 # print("for m=5: ",collusion_5(10,[4,4,3,2,1]))
 
 def collusion_5_method_2(N:int,shard_sizes: list[int]):
@@ -62,15 +89,11 @@ def collusion_5_method_2(N:int,shard_sizes: list[int]):
     return sum_
 # print("for m=5: ",collusion_5_method_2(10,[4,4,1,1,1]))
                         
-
-
-
 def collusion_m(N: int, shard_sizes: list[int]) -> float:
     m = len(shard_sizes)
     if m < 2:
         raise ValueError("Need at least 2 shards")
     
-    # Denominator = \prod_{j=2}^m binom{N}{n_j}
     denom = 1
     for i in range(1, m):
         denom *= comb(N, shard_sizes[i])
@@ -80,10 +103,8 @@ def collusion_m(N: int, shard_sizes: list[int]) -> float:
             # multiply by binom{n_{m-1}} {n_{m-1} + n_m - k_{m-2}}
             return prod * comb(shard_sizes[m-2], shard_sizes[m-2] + shard_sizes[m-1] - prev) 
             
-        # q_i = N - n1 - (k_1 + ... + k_{i-1})
-        q = N - shard_sizes[0] - sum_k
-        # p_i = sum_{j=i+1}^m n_j
-        p = sum(shard_sizes[level:])
+        q = N - shard_sizes[0] - sum_k # q_i = N - n1 - (k_1 + ... + k_{i-1})
+        p = sum(shard_sizes[level:]) # p_i = sum_{j=i+1}^m n_j
         
         # lower bound
         if level == 1:
@@ -110,6 +131,4 @@ def collusion_m(N: int, shard_sizes: list[int]) -> float:
 
     total = rec(1, N, 0, 1)
     return total / denom
-print("for m=4: ",collusion_m(10,[6,3,1,4]))
-
-
+# print("for m=4: ",collusion_m(10,[3,4,3,5]))
---
+++ scripts/plot_occ_collusion_2.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from math import comb, sqrt
+from mpl_toolkits.mplot3d import Axes3D  
+from occenv.constants import FIGURE_DIR 
+from scripts.collusion_prob import collusion_2
+
+def create_meshgrid(start=1, stop=10):
+    n_vals = np.arange(start, stop)
+    return np.meshgrid(n_vals, n_vals)
+
+def create_3d_plot(x, y, z, xlabel, ylabel, zlabel, title, filename):
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    surf = ax.plot_surface(x, y, z, cmap='viridis', edgecolor='none')
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.set_zlabel(zlabel)
+    ax.set_title(title)
+    fig.colorbar(surf, shrink=0.5, aspect=10, pad=0.1)
+    plt.savefig(FIGURE_DIR / filename)
+    plt.close(fig)
+
+def plot_collude_2():
+    N = 10
+    x, y = create_meshgrid()
+    z = np.vectorize(lambda n1, n2: collusion_2(N, [n1, n2]))(x, y)
+    create_3d_plot(
+        x, y, z,
+        '$n_1$', '$n_2$', 'Probability of Collusion',
+        'Probability of Collusion (m=2) with $N=10$', "collude_2.pdf"
+    )
+
+def plot_occ_2():
+    N = 10
+    x, y = create_meshgrid()
+    z = np.sqrt(x * y) / N
+    create_3d_plot(
+        x, y, z,
+        '$n_1$', '$n_2$', r'OCC when m=2',
+        r'3D Surface Plot of OCC when m=2', "occ_2.pdf"
+    )
+
+if __name__ == "__main__":
+    plot_collude_2()
+    plot_occ_2()
