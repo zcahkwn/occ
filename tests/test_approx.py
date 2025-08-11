608671be20a2528e7180ed54812fb5488ee9b322
Alice Ng <alice@Alices-MacBook-Air.local>
2025-08-11 18:09:50 +0100

    Update approximated results and tests

--- tests/test_approx.py
+++ tests/test_approx.py
@@ -22,10 +22,23 @@ def test_approx(total_number, shard_sizes):
     biv = AnalyticalBivariate(total_number, shard_sizes)
     approx = ApproximatedResult(total_number, shard_sizes)
 
+    # For union distribution, test mean, variance
     assert approx.union_mu_approx() == pytest.approx(univ.union_mu(), abs=0.01)
+    assert approx.union_var_approx() == pytest.approx(univ.union_var(), abs=0.01)
+
+    # For intersection distribution, test mean, variance
     assert approx.intersection_mu_approx() == pytest.approx(
         univ.intersection_mu(), abs=0.01
     )
+    assert approx.intersection_var_approx() == pytest.approx(
+        univ.intersection_var(), abs=0.01
+    )
+
+    # For bivariate distribution, test mean vector, and variance matrix
+    assert approx.bivariate_mu_approx() == pytest.approx(biv.bivariate_mu(), abs=0.01)
+    assert approx.bivariate_matrix_approx() == pytest.approx(
+        biv.bivariate_matrix(), abs=0.01
+    )
+
+    # For jaccard index, test mean
     assert approx.jaccard_mu_approx() == pytest.approx(biv.jaccard_mu(), abs=0.01)
-    # assert approx.union_var_approx() == ana.union_var_approx()
-    # assert approx.union_sd_approx() == ana.union_sd_approx()
